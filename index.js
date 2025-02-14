efdb13a661b0922dd9f6825efada9465899c23f1
Surya Sir <82026189+suryavanshyadav@users.noreply.github.com>
2025-02-14 13:39:02 +0000

    new update for server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const { createServer } = require('node:http');
 
-if (timestamp < (serverTime + 1000) && (serverTime - timestamp) <= recvWindow) {
-  // process request
-} else {
-  // reject request
-}
+const hostname = '127.0.0.1';
+const port = 3000;
 
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
 
-// new code will addedd here
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
